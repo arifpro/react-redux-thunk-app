6692db844c97aed45b246e7500a460d578b663ee
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-12-30 19:51:54 +0800

    todo list

--- src/components/ToDo/TodoItem.js
+++ src/components/ToDo/TodoItem.js
@@ -2,15 +2,16 @@ import React from 'react'
 
 const TodoItem = () => {
     return (
-        <div>
-            <h3>TodoItem</h3>
-            <div className="row">
-                <div>#1</div>
-                <div>Title</div>
-                <button>Edit</button>
-                <button>Delete</button>
-            </div>
-        </div>
+        <tr>
+            <td data-label="No">#1</td>
+            <td data-label="Title">24</td>
+            <td data-label="Edit" onClick={() => null} style={{ color: 'blue' }}>
+                <i class="edit icon"></i>
+            </td>
+            <td data-label="Delete" onClick={() => null} style={{ color: 'red' }}>
+                <i class="trash icon"></i>
+            </td>
+        </tr>
     )
 }
 
--- src/components/ToDo/TodoList.js
+++ src/components/ToDo/TodoList.js
@@ -4,7 +4,28 @@ import TodoItem from './TodoItem'
 const TodoList = () => {
     return (
         <div>
-            <TodoItem />
+            <section style={{ margin: '3rem' }}>
+                <h3>TodoItem</h3>
+                <table class="ui celled table">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Title</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <TodoItem />
+                        <TodoItem />
+                        <TodoItem />
+                        <TodoItem />
+                        <TodoItem />
+                        <TodoItem />
+                        <TodoItem />
+                    </tbody>
+                </table>
+            </section>
         </div>
     )
 }
